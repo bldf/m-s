f289d18c0897e319d97b70669f421cc695bbf650
百里登风 <bldf@qq.com>
2020-02-15 16:19:37 +0800

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,6 +1,6 @@
 const path = require('path') ;
 function init(router,send){
-    router.get('/',async ctx=>{//主页， 使用render，当页面改变后不会立即刷新，因为在初始化加载了swig模板引擎
+    router.get('/',async ctx=>{//主页， 使用render，当页面改变后不会立即刷新，因为在初始化加载了swig模板引擎,默认配置了缓存
         ctx.body =  await ctx.render('/index.html');
     });
     // router.get('/pages', async ctx => {//通过文件夹的方式去加载页面
